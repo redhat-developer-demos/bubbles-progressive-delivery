1cffc1bf9a7c2db85ad1073b50ba1d31ca3c5fc2
Kevin Dubois <kevin.dubois@gmail.com>
2023-03-28 16:05:54 +0200

    split rollouts with and without analytics

--- bubbles-frontend/src/main/java/org/acme/Statistics.java
+++ bubbles-frontend/src/main/java/org/acme/Statistics.java
@@ -1,8 +1,6 @@
 package org.acme;
 
-import java.util.ArrayList;
 import java.util.HashSet;
-import java.util.List;
 import java.util.Map;
 import java.util.Set;
 import java.util.concurrent.ConcurrentHashMap;

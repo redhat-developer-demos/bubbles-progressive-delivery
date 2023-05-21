49a23c6c2348fc8b610e6944bba5c4b98668546c
Kevin Dubois <kevin.dubois@gmail.com>
2023-05-21 16:58:41 +0200

    update paths, metrics

--- scripts/argorollouts/withmetrics/deploy.sh
+++ scripts/argorollouts/withmetrics/deploy.sh
@@ -2,20 +2,22 @@
 
 echo "###### Deploying FrontEnd ######"
 
-kubectl apply -f ../../bubbles-frontend/src/main/argorollouts/Deployment.yaml
-kubectl apply -f ../../bubbles-frontend/src/main/argorollouts/Service.yaml
+kubectl apply -f ../../../bubbles-frontend/src/main/argorollouts/Deployment.yaml
+kubectl apply -f ../../../bubbles-frontend/src/main/argorollouts/Service.yaml
 
 kubectl wait --for=condition=available --timeout=600s deployment/bubblefrontend
 
-kubectl apply -f ../../bubbles-frontend/src/main/argorollouts/Gateway.yaml
+kubectl apply -f ../../../bubbles-frontend/src/main/argorollouts/Gateway.yaml
 
 echo "###### Deploying BackEnd V1 ######"
 
-kubectl apply -f ../../bubbles-backend/src/main/argorollouts-metrics/Service.yaml
-kubectl apply -f ../../bubbles-backend/src/main/argorollouts-metrics/Service-canary.yaml
-kubectl apply -f ../../bubbles-backend/src/main/argorollouts-metrics/virtual-service-bubble-backend-v1_and_v2_100_0.yaml
+kubectl apply -f ../../../bubbles-backend/src/main/argorollouts-metrics/Service.yaml
+kubectl apply -f ../../../bubbles-backend/src/main/argorollouts-metrics/Service-canary.yaml
+kubectl apply -f ../../../bubbles-backend/src/main/argorollouts-metrics/virtual-service-bubble-backend-v1_and_v2_100_0.yaml
 
-kubectl apply -f ../../bubbles-backend/src/main/argorollouts-metrics/rollout-metrics.yaml
+kubectl apply -f ../../../bubbles-backend/src/main/argorollouts-metrics/rollout-metrics.yaml
+kubectl apply -f ../../../bubbles-backend/src/main/argorollouts-metrics/kustomization.yaml
+kubectl apply -f ../../../bubbles-backend/src/main/argorollouts-metrics/analysistemplate.yaml
 
 kubectl get routes -n istio-system
 

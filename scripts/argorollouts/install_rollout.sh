c478044764f1015a324c4ccf082f481840980d1d
Kevin Dubois <kevin.dubois@gmail.com>
2023-06-03 17:29:54 +0200

    change to green

--- scripts/argorollouts/install_rollout.sh
+++ scripts/argorollouts/install_rollout.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 kubectl create namespace argo-rollouts
-kubectl apply -n argo-rollouts -f https://github.com/argoproj/argo-rollouts/releases/download/v1.2.1/install.yaml
+kubectl apply -n argo-rollouts -f https://github.com/argoproj/argo-rollouts/releases/download/v1.5.1/install.yaml
